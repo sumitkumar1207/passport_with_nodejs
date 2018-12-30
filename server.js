52612c90ab869fd0e8509d5d2778f97f3ee53dd5
sumitkumar <sumit@binarynumbers.io>
2018-12-30 13:36:03 +0530

    In the UI welcome and login and register page are finshed

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 //Initialize app to express
 const app = express();
@@ -9,6 +13,24 @@ const db = require('./config/keys').mongoURI;
 
 //Connect to DB
 mongoose.connect(db, {useNewUrlParser: true}).then(console.log("MongoDb connected..")).catch(err=>console.log(err))
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs')
+
+//Express body parser
+app.use(express.urlencoded({extended: true}));
+
+//Exoress session
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+}));
+
+//Routes
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
 
 //Creating port
 const PORT = process.env.PORT || 5505;
