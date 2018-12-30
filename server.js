431879a7d7533eee22eb9a6001961792b336f224
sumitkumar <sumit@binarynumbers.io>
2018-12-30 12:36:13 +0530

    Connecting to db successfully

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //Initialize app to express
 const app = express();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to DB
+mongoose.connect(db, {useNewUrlParser: true}).then(console.log("MongoDb connected..")).catch(err=>console.log(err))
 
 //Creating port
 const PORT = process.env.PORT || 5505;
